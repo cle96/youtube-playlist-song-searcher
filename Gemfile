9306c90ea1b132e0f42080ce22e738c47adb37b0
Leahu Cristian <puiutzu96@yahoo.com>
2017-04-27 22:17:13 +0200

    finished the playlist retriever.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'google-api-client', '0.7'
+gem 'retriable', '2.1.0'
--- main.rb
+++ main.rb
@@ -2,51 +2,70 @@ require 'rubygems'
 gem 'google-api-client', '0.7'
 require 'google/api_client'
 
-class Main
-  options = ARGV
+options = ARGV
 
-  YOUTUBE_API_KEY = File.read('api_key')
-  YOUTUBE_API_SERVICE_NAME = 'youtube'
-  YOUTUBE_API_VERSION = 'v3'
+YOUTUBE_API_KEY = File.read('api_key')
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
-  def self.get_service
-    client = Google::APIClient.new(
-        :key => YOUTUBE_API_KEY,
-        :authorization => nil,
-        :application_name => $PROGRAM_NAME,
-        :application_version => '1.0.0'
-    )
-    youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
-
-    return client, youtube
-  end
+def get_service
+  client = Google::APIClient.new(
+      :key => YOUTUBE_API_KEY,
+      :authorization => nil,
+      :application_name => $PROGRAM_NAME,
+      :application_version => '1.0.0'
+  )
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
 
-  def self.fetchList
-    client = get_service
-    search_response = client.execute!(
-        :api_method => youtube.search.list,
+  return client, youtube
+end
+
+
+def main
+  client, youtube = get_service
+
+  begin
+    uploads_list_id = File.read('upload_lists')
+
+    playlistitems_response = client.execute!(
+        :api_method => youtube.playlist_items.list,
         :parameters => {
-            :part => 'snippet'
+            :playlistId => uploads_list_id,
+            :part => 'snippet',
         }
     )
 
-    videoList = []
+    next_page_token = ''
+    videos = []
+    until next_page_token.nil?
+      playlistitems_response = client.execute!(
+          :api_method => youtube.playlist_items.list,
+          :parameters => {
+              :playlistId => uploads_list_id,
+              :part => 'snippet',
+              :maxResults => 50,
+              :pageToken => next_page_token
+          }
+      )
 
-    search_response.data.items.each do |search_result|
-      case search_result.id.kind
-        when 'youtube#video'
-          videoList << "#{search_result.snippet.title}"
+
+      # Print information about each video.
+
+      playlistitems_response.data.items.each do |playlist_item|
+        title = playlist_item['snippet']['title']
+        videos.push("#{title}")
       end
+
+
+      next_page_token = playlistitems_response.next_page_token
     end
-
-    puts "Videos:\n", videoList, "\n"
-
+    puts "Videos:\n", videos, "\n"
   end
+rescue Google::APIClient::TransmissionError => e
+  puts e.result.body
 
-  playlists = fetchList
-
-  puts playlists
-  puts options
-
+  return videos
 end
+
+main
